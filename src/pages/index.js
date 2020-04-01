47d87fccd6e5d09b3754db45701f23d387d4619f
hashimoto_k <khash06@gmail.com>
2020-03-31 21:32:03 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <>
+        <h1>This App Rules!</h1>
+        <p>Log in to find out why.</p>
+        <Link to="/dashboard">Go to dashboard</Link>
+    </>
+);
