cfcb200bfb1bed375163d6ea338bd8b53bc03a05
hashimoto_k <khash06@gmail.com>
2020-04-03 23:28:47 -0700

    add login modal

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Router } from '@reach/router';
+import IdentityModal from 'react-netlify-identity-widget';
 import Layout from '../components/layout';
 import Profile from '../components/profile';
 import RouteBase from '../components/route-base';
@@ -7,12 +8,15 @@ import RouteSecret from '../components/route-secret';
 import RouteLogin from '../components/route-login';
 import { navigate } from 'gatsby';
 
+import 'react-netlify-identity-widget/styles.css';
+
 const Dashboard = ({ location }) => {
+    const [isVisible, setVisibility] = useState(false);
     useEffect(() => {
         if(location.pathname.match(/^\/dashboard\/?$/)) {
             navigate('dashboard/login', {replace: true})
         }
-    }, []);
+    });
     return(
         <Layout>
             <Profile />
@@ -21,6 +25,9 @@ const Dashboard = ({ location }) => {
                 <RouteSecret path="/dashboard/secret"/>
                 <RouteLogin path="/dashboard/login"/>
             </Router>
+            <IdentityModal 
+                showDialog={isVisible} onCloseDialog={() => setVisibility(false)}
+            />
         </Layout>
     );
 };
